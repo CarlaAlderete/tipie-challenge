1c57ae6a2ba42dc002d4f3e81c4d50718dda68f6
Carla Alderete <carlamalderete@gmail.com>
2021-12-30 14:41:23 -0300

    -

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css'
 import { useState } from 'react'
-import {Navigate, Route, Routes} from 'react-router-dom'
+import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import SignIn from './pages/SignIn'
@@ -12,11 +12,13 @@ const App = ()=>{
   return(
     <>
     <Header/>
-    <Routes>
-      {!tokenUser &&<Route path="/" element={<SignIn setTokenUser={setTokenUser}/>}/>}
-      {tokenUser && <Route path="/" element={<Home/>}/>}
-      <Route path="*" element={<Navigate to="/" />}/>  
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        {!tokenUser &&<Route path="/" element={<SignIn setTokenUser={setTokenUser}/>}/>}
+        {tokenUser && <Route path="/" element={<Home/>}/>}
+        <Route path="*" element={<Navigate to="/" />}/>  
+      </Routes>
+    </BrowserRouter>
     <Footer/>
     </>
   )
